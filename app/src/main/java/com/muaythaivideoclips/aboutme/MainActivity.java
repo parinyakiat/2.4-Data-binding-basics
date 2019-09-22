950d1ac67b2f20073b92b9685ad65d00f679e30e
TheNineInwKung <nine4567@gmail.com>
2019-09-22 20:59:11 +0700

    Begin Project

---
+++ app/src/main/java/com/muaythaivideoclips/aboutme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.muaythaivideoclips.aboutme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //test
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
