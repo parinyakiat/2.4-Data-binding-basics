e1ee51b0c02c504add1a1faa2471524ab13a6a2a
TheNineInwKung <nine4567@gmail.com>
2019-09-22 21:03:36 +0700

    fix

--- app/src/main/java/com/muaythaivideoclips/aboutme/MainActivity.java
+++ app/src/main/java/com/muaythaivideoclips/aboutme/MainActivity.java
@@ -8,7 +8,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
